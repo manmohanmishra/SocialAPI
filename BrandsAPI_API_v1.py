d787a3b5b6678269192314255027f1b046284d1b
Mishra <manmohan.a.mishra@accenture.com>
2018-12-11 18:33:03 +0400

    Initial commit - Brandseye - No Autdetails

---
+++ BrandsAPI_API_v1.py
@@ -0,0 +1,17 @@
+import requests
+ 
+#date_after="2018//09//01"
+#date_before="2018/12/01"
+# Sample Basic Auth Url with login values as username and password
+
+# Make a request to the endpoint using the correct auth values
+auth_values = (user, passwd)
+response = requests.get(url, auth=auth_values)
+print(response) 
+# Convert JSON to dict and print
+#print(response.json())
+data = response.json()
+print(data)
+
+#import pandas as pd
+data.to_csv("myfile.csv")
