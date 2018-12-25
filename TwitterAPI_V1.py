a7e4b4a84e18c6b7adb9a418679d33b15488d720
Mishra <manmohan.a.mishra@accenture.com>
2018-12-25 11:39:59 +0400

    API-Tiwtter - Timeline Pull V1.0

---
+++ TwitterAPI_V1.py
@@ -0,0 +1,25 @@
+import tweepy
+from time import sleep
+from datetime import datetime
+#Authentication
+consumer_key = 'jjJkmq1vwdBYbRrVRSdtpA'
+consumer_secret = 'Enter Here'
+access_token = 'Enter Here'
+access_token_secret = 'Enter Here'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print (tweet.text)
+    print(tweet.created_at)
+    print(tweet.id)
+    #print (tweet)
+    
+    break
+
+#user = api.me()
+#print (user.name)
